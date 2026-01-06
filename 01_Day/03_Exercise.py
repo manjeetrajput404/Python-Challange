136ab4e4c0b88c0bb47d90ed0c22e28838e53d3f
Manjeet <manjeet@gmail.com>
2026-01-06 21:18:17 +0530

    Day 1

---
+++ 01_Day/03_Exercise.py
@@ -0,0 +1,8 @@
+glass1 = "milk"
+glass2 = "juice"
+print(f"glass 1: {glass1} \t glass 2: {glass2}")
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+print(f"glass 1: {glass1} \t glass 2: {glass2}")
---
+++ 01_Day/task.py
@@ -0,0 +1,15 @@
+# print("Hello" , input("What is your name?"), ".")
+
+# name = input("Enter your First Name\t")
+# name2 = input("Enter your Last Name\t")
+
+
+# print(f"My name is {name} {name2}")
+
+# print(f"{len(name)}, {len(name2)}")
+
+
+username = input("Enter username: ")
+length = len(username)
+
+print(f"The username is: {username} \n The Length is: {length}")
