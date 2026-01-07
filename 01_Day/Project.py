15dd4e4df38460247221439e4d3fc25f0e92013c
Manjeet <manjeet@gmail.com>
2026-01-07 19:55:55 +0530

    Day 2

--- 01_Day/Project.py
+++ 01_Day/Project.py
@@ -1,8 +1,26 @@
+# Project 1:
+# ----------------------------------------------------------------------------
 # Band name generator
 
-print("Welcome to Band Name Generator")
+# print("Welcome to Band Name Generator")
 
-city_name = input("What's the name of the city you grew up in?\n")
-pet_name = input("What's your pet's name?\n")
+# city_name = input("What's the name of the city you grew up in?\n")
+# pet_name = input("What's your pet's name?\n")
 
-print(f"Your band name could be {city_name} {pet_name}")
+# print(f"Your band name could be {city_name} {pet_name}")
+# ------------------------------------------------------------------------------
+
+# _____________________________________________________________________________
+
+# Project 2
+name = input("Enter your Name:\n")
+city = input("Enter your City:\n")
+hobby = input("Enter you favorite Hobby\n")
+
+print(f"""
+-------------------
+Name: {name}
+City: {city}
+Hobby: {hobby}
+-------------------
+""")
---
+++ 02_Day/task.py
@@ -0,0 +1,17 @@
+# # print(123_456_45)
+
+# name = "Manjeet"
+# roll_no = 29
+# phone = 9_518_818_658
+# gpa = 7.11
+# isAdult = True
+
+# print(type(name))
+# print(type(roll_no))
+# print(type(phone))
+# print(type(gpa))
+# print(type(isAdult))
+
+
+
+print("Number of letter in your name: " + str(len(input("Enter your name"))))
